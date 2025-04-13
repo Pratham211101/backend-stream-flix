50f3366c50b15a7cff343366ed092aed24467401
Pratham Goswami <goswamipratham2111200@gmail.com>
2025-04-13 14:43:22 +0530

    sameSite in constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const DB_NAME='vidtube'
 export const options={
     httpOnly:true,
-    secure:process.env.NODE_ENV==="production"
+    secure:process.env.NODE_ENV==="production",
+    sameSite:process.env.NODE_ENV==="production"?"none":"lax",
 }
