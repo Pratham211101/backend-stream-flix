558f1d4fe52ab5e28f291d21dafc1b64f8b0a785
Pratham Goswami <goswamipratham2111200@gmail.com>
2025-04-13 13:58:22 +0530

    bodyparser

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-
+const bodyParser = require("body-parser"); 
+router.use(bodyParser.json());
 const app=express()
 
 app.use(
